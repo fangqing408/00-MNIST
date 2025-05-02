1e8cd1cfce01149e758dfef0123a750094d5f2f2
fangqing408 <96216206+fangqing408@users.noreply.github.com>
2025-05-02 15:18:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import init
 import forward_pass
 import backward_pass
 import save_model
+from tqdm import tqdm
 def train(images, labels, epochs, batch_size, decay, learning_rate, images_test, labels_test):
     conv_kernel1, conv_kernel2, fc_weights, fc_bias = init.init()
     for epoch in range(epochs):
@@ -14,7 +15,7 @@ def train(images, labels, epochs, batch_size, decay, learning_rate, images_test,
         np.random.shuffle(indices)
         total_loss = 0.0
         correct_predictions = 0
-        for i in range(0, len(images), batch_size):
+        for i in tqdm(range(0, len(images), batch_size), desc="Training"):
             batch_indices = indices[i:i + batch_size]
             d_kernel1_sum = np.zeros_like(conv_kernel1)
             d_kernel2_sum = np.zeros_like(conv_kernel2)
@@ -48,7 +49,7 @@ def train(images, labels, epochs, batch_size, decay, learning_rate, images_test,
         avg_loss = total_loss / len(images)
         accuracy_train = correct_predictions / len(images)
         correct_predictions = 0
-        for i in range(len(images_test)):
+        for i in tqdm(range(len(images_test)), desc="Testing"):
             image_test = images_test[i]
             label_test = labels_test[i]
             (convolved1, activated1, pooled1, pool_indices1, convolved2, activated2,
@@ -67,8 +68,8 @@ if __name__ == "__main__":
     image_size = 28
     paths, images, labels = load_images.load_data('./train', classes, image_size, samples)
     paths_test, images_test, labels_test = load_images.load_data('./test', classes, image_size, samples // 10)
-    epochs = 10
-    batch_size = 16
+    epochs = 20
+    batch_size = 10
     decay = 0.95
     learning_rate = 0.003
     train(images, labels, epochs, batch_size, decay, learning_rate, images_test, labels_test)
